5c4206801123ae9ee3bdaa7a91080dccbc0dfb54
keshavvamsig-26 <g.keshavvamsi@gmail.com>
2025-06-07 18:52:20 +0530

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About GradeGen</h1>
+      <p>This tool helps students track GPA and CGPA with What-If analysis.</p>
+    </div>
+  );
+}
